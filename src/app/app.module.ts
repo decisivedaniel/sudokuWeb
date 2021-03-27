41f32b1e2a8149c8de06b0a0a5874d3cbff57629
Dan Roesch <danielar2143@gmail.com>
2021-03-26 22:42:57 -0500

    made a proto-sudoku column, needs styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +15,7 @@ import { SudokuComponent } from './sudoku/sudoku.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
