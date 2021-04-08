a380ce3706948ff848c86edb4b3c6b9f53bafe45
Dan Roesch <danielar2143@gmail.com>
2021-04-07 20:15:23 -0500

    Test uniqueness columns and rows; test num range

--- src/app/sudoku.service.spec.ts
+++ src/app/sudoku.service.spec.ts
@@ -25,42 +25,69 @@ fdescribe('SudokuService', () => {
 });
 
 function checkValidityOfGrid(grid : Cell[][]): void {
-  var issues : number = 0;
-  issues += checkSections(grid);
-  issues += checkRows(grid);
-  issues += checkColumns(grid);
-  expect(issues).withContext("valid sudoku grids will have zero repeats per grouping, found").toEqual(0);  
+  let invalidRangeIssues : number = 0;
+  let duplicateIssues  : number = 0;
+
+  invalidRangeIssues += checkOutOfRange(grid);
+
+  duplicateIssues += checkSections(grid);
+  duplicateIssues += checkRows(grid);
+  duplicateIssues += checkColumns(grid);
+
+  expect(invalidRangeIssues).withContext("valid numbers will be between 1-9, number of cells were out of range").toBe(0)
+  expect(duplicateIssues).withContext("valid sudoku grids will have zero repeats per grouping, found").toBe(0);  
 };
 
-function checkSections(grid : Cell[][]) : number{
-  var issues : number = 0;
-  for (var section of grid){
-    issues += checkUniqueNumber(section.map(cell => cell.correctNumber));
+function checkOutOfRange(grid : Cell[][]) : number{
+  let issues : number = 0;
+  for (let group of grid){
+    for (let check of group){
+      if(check.correctNumber < 1 || check.correctNumber > 9){
+        issues++;
+      }
+    }
   }
-  return issues
+  return issues;
+}
+
+function checkSections(grid : Cell[][]) : number{
+  return checkUniqueNumber(grid.map(section => section.map(cell => cell.correctNumber)));
 };
 
 function checkRows(grid : Cell[][]) : number{
-  var issues : number = 0;
   //transform sections -> rows
-  
-  //run checkUniqueNumber
-  return issues
+  let rowTransformedGrid : number[][] = [[],[],[],[],[],[],[],[],[]];
+  for (let section : number = 0; section < 9; section++){
+    for (let cell : number = 0; cell < 9; cell++){
+      //Need to convert 9 sections arrays into similar sized 9 rows arrays
+      let cellsNewRow : number = (((Math.floor(section / 3)) * 3)+Math.floor(cell / 3));
+      rowTransformedGrid[cellsNewRow].push(grid[section][cell].correctNumber);
+    }
+  }
+  return checkUniqueNumber(rowTransformedGrid);
 };
 
 function checkColumns(grid : Cell[][]) : number{
-  var issues : number = 0;
   //transform sections -> columns
-  
-  //run checkUniqueNumber
-  return issues;
+  let columnTransformedGrid : number[][] = [[],[],[],[],[],[],[],[],[]];
+  for (let section : number = 0; section < 9; section++){
+    for (let cell : number = 0; cell < 9; cell++){
+      //Need to convert 9 sections arrays into similar sized 9 columns arrays
+      let cellsNewSection : number = (((section % 3) + 3)+(cell % 3));
+      columnTransformedGrid[cellsNewSection].push(grid[section][cell].correctNumber);
+    }
+  }
+  return checkUniqueNumber(columnTransformedGrid);
+
 };
 
-function checkUniqueNumber(group : number[]) : number {
-  var issues : number = 0;
-  for (var check of group){
-    if(group.indexOf(check) != group.lastIndexOf(check)){
-      issues++;
+function checkUniqueNumber(grid : number[][]) : number {
+  let issues : number = 0;
+  for (let group of grid){
+    for (let check of group){
+      if(group.indexOf(check) != group.lastIndexOf(check)){
+        issues++;
+      }
     }
   }
   return issues;
